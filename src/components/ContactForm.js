a0677d0fb956da9ccd6e8875f6fe2cc8d636c009
Object-ions <moshikoatia@gmail.com>
2024-04-18 22:20:16 -0700

    Add ContactForm layout and content

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,44 @@
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <div className="contact-form">
+      <div className="contact-container">
+        <div className="form-wrapper">
+          <h2>Logo</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+            magnam consequuntur temporibus illum magni. Voluptatibus
+            exercitationem quos maiores maxime officia.
+          </p>
+          <form>
+            <input
+              type="text"
+              id="name"
+              placeholder="Full Name"
+              aria-label="Full Name"
+            />
+
+            <input
+              type="email"
+              id="email"
+              placeholder="Email Address"
+              aria-label="Email Address"
+            />
+
+            <input
+              type="tel"
+              id="phone-number"
+              placeholder="Phone Number"
+              aria-label="Phone Number"
+            />
+
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactForm;
