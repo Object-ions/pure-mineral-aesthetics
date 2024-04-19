3770679d6ce9c2ea843acdcb6b7c7c9be72bb770
Object-ions <moshikoatia@gmail.com>
2024-04-19 09:50:41 -0700

    Update Testimonials and its layout

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,7 +2,11 @@ import testimonialsImg from '../assets/jpg/testimonials.png';
 const Testimonials = () => {
   return (
     <div className="testimonials">
-      <img src={testimonialsImg} alt="testimonials" />;
+      <div className="testimonials-container">
+        <div className="testimonials-image-wrapper">
+          <img src={testimonialsImg} alt="testimonials" />
+        </div>
+      </div>
     </div>
   );
 };
