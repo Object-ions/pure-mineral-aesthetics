d120e664d5621cba6088665d7c5f2a5ac25d0ce8
Object-ions <moshikoatia@gmail.com>
2024-04-18 20:45:58 -0700

    Adjust container sizing and bg

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -3,46 +3,48 @@ import { Link } from 'react-router-dom';
 const ServicesSection = () => {
   return (
     <div className="services-section">
-      <h3>Our Services</h3>
-      <div className="service-card">
-        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
-        <h4>Signature Facial</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
-          eget euismod arcu risus vitae justo.
-        </p>
+      <div className="services-container">
+        <h3>Our Services</h3>
+        <div className="service-card">
+          <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+          <h4>Signature Facial</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+            eget euismod arcu risus vitae justo.
+          </p>
+        </div>
+        <div className="service-card">
+          <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+          <h4>Signature Facial</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+            eget euismod arcu risus vitae justo.
+          </p>
+        </div>
+        <div className="service-card">
+          <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+          <h4>Signature Facial</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+            eget euismod arcu risus vitae justo.
+          </p>
+        </div>
+        <div className="service-card">
+          <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+          <h4>Signature Facial</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+            eget euismod arcu risus vitae justo.
+          </p>
+        </div>
+        <Link to={'/'} className="btn">
+          Explore All Out Treatments
+        </Link>
       </div>
-      <div className="service-card">
-        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
-        <h4>Signature Facial</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
-          eget euismod arcu risus vitae justo.
-        </p>
-      </div>
-      <div className="service-card">
-        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
-        <h4>Signature Facial</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
-          eget euismod arcu risus vitae justo.
-        </p>
-      </div>
-      <div className="service-card">
-        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
-        <h4>Signature Facial</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
-          eget euismod arcu risus vitae justo.
-        </p>
-      </div>
-      <Link to={'/'} className="btn">
-        Book Your Appointment Now
-      </Link>
     </div>
   );
 };
