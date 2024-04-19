cb450e5e29df0e37686a54fd23762c6f35a81bc0
Object-ions <moshikoatia@gmail.com>
2024-04-19 09:13:39 -0700

    Add Testimonials and assets

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,10 @@
+import testimonialsImg from '../assets/jpg/testimonials.png';
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="testimonials">
+      <img src={testimonialsImg} alt="testimonials" />;
+    </div>
+  );
 };
 
 export default Testimonials;
