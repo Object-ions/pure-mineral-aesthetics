20a06af61d3efa381ef821f06d7b3bc73a81edca
Object-ions <moshikoatia@gmail.com>
2024-04-18 20:42:31 -0700

    Add Services section layout and content

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,5 +1,50 @@
+import { Link } from 'react-router-dom';
+
 const ServicesSection = () => {
-  return <div>ServicesSection</div>;
+  return (
+    <div className="services-section">
+      <h3>Our Services</h3>
+      <div className="service-card">
+        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+        <h4>Signature Facial</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+          eget euismod arcu risus vitae justo.
+        </p>
+      </div>
+      <div className="service-card">
+        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+        <h4>Signature Facial</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+          eget euismod arcu risus vitae justo.
+        </p>
+      </div>
+      <div className="service-card">
+        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+        <h4>Signature Facial</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+          eget euismod arcu risus vitae justo.
+        </p>
+      </div>
+      <div className="service-card">
+        <div className="image-wrapper">{/* <img src="" alt="" /> */}</div>
+        <h4>Signature Facial</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          efficitur, massa ultricies aliquet iaculis, ante leo tempor lorem,
+          eget euismod arcu risus vitae justo.
+        </p>
+      </div>
+      <Link to={'/'} className="btn">
+        Book Your Appointment Now
+      </Link>
+    </div>
+  );
 };
 
 export default ServicesSection;
