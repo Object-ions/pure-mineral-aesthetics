9f180056488a47444b16fd91caf8ce204bd24c26
Object-ions <moshikoatia@gmail.com>
2024-04-19 18:23:16 -0700

    Add assets, update CTA header

--- src/components/layout/TopCTA.js
+++ src/components/layout/TopCTA.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as FacebookIcon } from '../../assets/svg/facebook.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/svg/instagram.svg';
+import { ReactComponent as LocationIcon } from '../../assets/svg/location.svg';
+import { ReactComponent as CallIcon } from '../../assets/svg/call.svg';
 
 const TopCTA = () => {
   return (
     <div className="topCTA">
       <div className="navbar-container">
         <ul className="ul-row">
-          <li>X</li>
+          <li>
+            <CallIcon className="svg-icon" />
+          </li>
           <li>972-353-4623</li>
-          <li>X</li>
+          <li>
+            <LocationIcon className="svg-icon" />
+          </li>
           <li>8700 E Pinnacle Peak Rd. Suite 101 Scottsdale AZ</li>
         </ul>
         <ul className="ul-row">
           <li>
-            <Link to="/">Facebook</Link>
+            <Link to="/">
+              <FacebookIcon className="svg-icon" />
+            </Link>
           </li>
           <li>
-            <Link to="/">Instagram</Link>
+            <Link to="/">
+              <InstagramIcon className="svg-icon" />
+            </Link>
           </li>
         </ul>
       </div>
