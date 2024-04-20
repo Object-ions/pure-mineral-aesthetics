61aaf40ec85bdf1970ecf595a12fbb1b39c07794
Object-ions <moshikoatia@gmail.com>
2024-04-19 18:51:25 -0700

    Update Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,22 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as ArrowIcon } from '../assets/svg/arrow_forward.svg';
+
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-container">
         <div className="hero-text">
-          <h2>Elevate your skin at Pure Mineral Aesthetics</h2>
+          <h2>Revitalize Your Essence with Pure Mineral Aesthetics</h2>
           <p>
-            Experience a World of Radiant, Youthful Skin Tailored Exclusively
-            for You
+            Discover the harmony of nature and luxury. Our exclusive
+            mineral-based skincare treatments are designed to rejuvenate your
+            skin and enhance your natural beauty. Experience the touch of
+            pureness and sophistication in every session.
           </p>
-          <button className="btn">Book Your Appointment</button>
+          <Link to="/" className="btn">
+            <span>Explore Treatments</span>
+            <ArrowIcon className="svg-icon" />
+          </Link>
         </div>
 
         <div className="hero-image"></div>
