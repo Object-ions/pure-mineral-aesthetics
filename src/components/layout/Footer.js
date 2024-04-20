a1cdc3056d7069a5df90f269c12e4311d540011f
Object-ions <moshikoatia@gmail.com>
2024-04-19 22:54:35 -0700

    Update Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,22 +1,39 @@
 import { Link } from 'react-router-dom';
 import Copyrights from './Copyrights';
+import { ReactComponent as FacebookIcon } from '../../assets/svg/facebook.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/svg/instagram.svg';
+import { ReactComponent as TwitterIcon } from '../../assets/svg/x-twitter.svg';
+
 const Footer = () => {
+  const logo = '/images/mineral-logo.png';
+  const logotype = '/images/mineral-logotype.png';
   return (
     <footer>
       <div className="footer">
         <div className="visit-us">
           <h3>Visit Us</h3>
           <p>
-            Lorem ipsum dolor sit amet, <br />
-            consectetur adipisicing elit.
+            4023 18th St, Ste B,
+            <br />
+            San-Francisco, CA 94114
           </p>
-          <p>152-874-9852</p>
+          <p>415-874-9852</p>
           <>
-            <Link to={'/'}>FB</Link>
-            <Link to={'/'}>IG</Link>
+            <Link to={'/'}>
+              <InstagramIcon className="svg-icon" />
+            </Link>
+            <Link to={'/'}>
+              <FacebookIcon className="svg-icon" />
+            </Link>
+            <Link to={'/'}>
+              <TwitterIcon className="svg-icon" />
+            </Link>
           </>
         </div>
-        <div className="logo">Logo</div>
+        <div className="footer-logos">
+          <img className="logo" src={logo} alt="logo" />
+          <img className="logotype" src={logotype} alt="logo" />
+        </div>
         <div className="navigation">
           <Link to={'/'}>Testimonials</Link>
           <Link to={'/'}>About</Link>
