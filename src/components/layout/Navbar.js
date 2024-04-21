468c8644944fc00724b183736fef22dbe1621882
Object-ions <moshikoatia@gmail.com>
2024-04-21 16:48:23 -0700

    Add responsiveness to Navbar, hamburger menu to navbar including assets

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,43 +1,38 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TopCTA from './TopCTA';
+import hamburger from '../../assets/svg/hamburger-icon.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const logo = '/images/mineral-logotype.png';
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header>
       <TopCTA />
       <div className="navbar">
         <div className="navbar-container">
-          <ul className="ul-row">
-            <li className="logotype">
-              <Link to="/">
-                <img src={logo} alt="Mineral Logo" className="logotype" />
-              </Link>
-            </li>
-          </ul>
-          <ul className="ul-row">
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/treatments">Treatments</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact-us">Contact Us</Link>
-            </li>
-          </ul>
-          <ul className="ul-row">
-            <li>
-              <Link to="/sign-in">Sign In</Link>
-            </li>
-            <li>/</li>
-            <li>
-              <Link to="/sign-up">Sign Up</Link>
-            </li>
-          </ul>
+          <div className="navbar-logo">
+            <Link to="/">
+              <img src={logo} alt="Mineral Logo" className="logo" />
+            </Link>
+          </div>
+          <button className="menu-toggle" onClick={toggleMenu}>
+            <img src={hamburger} alt="Menu" />
+          </button>
+          <nav className={`navbar-links ${isOpen ? 'open' : ''}`}>
+            <Link to="/products">Products</Link>
+            <Link to="/treatments">Treatments</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact-us">Contact Us</Link>
+          </nav>
+          <div className={`navbar-profile ${isOpen ? 'open-inline' : ''}`}>
+            <Link to="/sign-in">Sign In</Link>
+            <span>/</span>
+            <Link to="/sign-up">Sign Up</Link>
+          </div>
         </div>
       </div>
     </header>
