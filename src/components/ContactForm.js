5d98d4e5456e3aff585cd827321e24b2bcd00cde
Object-ions <moshikoatia@gmail.com>
2024-04-21 08:48:58 -0700

    Add Leaflet component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,28 @@
+import { useLocation } from 'react-router-dom';
+import MapComponent from './MapComponent';
+
 const ContactForm = () => {
+  const location = useLocation();
+
+  // Leaflet configuration
+  const center = [37.761222, -122.434559]; // Coordinates for the address
+  const zoom = 17; // Zoom level
+  const mapStyle = { height: '400px', width: '75%' }; // Style for the map
+
   const logo = '/images/mineral-logo.png';
   return (
     <div className="contact-form">
       <div className="contact-container">
         <div className="form-wrapper">
           <img src={logo} alt="logo" className="logo" />
+          {location.pathname === '/contact-us' && (
+            <div className="contact-us">
+              <p>Via call, email or address</p>
+              <p>hello@minerals.com</p>
+              <p>Phone: 972-353-4623</p>
+              <p>Address: 4023 18th St, Ste B, San Francisco, CA 94114</p>
+            </div>
+          )}
           <h2>Get in Touch</h2>
           <p>
             Ready to embark on a journey to pure, radiant skin? Contact us to
@@ -38,6 +56,12 @@ const ContactForm = () => {
             </button>
           </form>
         </div>
+
+        <br />
+        <br />
+        {location.pathname === '/contact-us' && (
+          <MapComponent center={center} zoom={zoom} style={mapStyle} />
+        )}
       </div>
     </div>
   );
---
+++ src/components/MapComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+const center = [37.761222, -122.434559]; // Latitude and Longitude of the address
+
+const MapComponent = () => {
+  return (
+    <MapContainer
+      center={center}
+      zoom={17}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={center}>
+        <Popup>
+          Pure Mineral Aesthetics <br /> 4023 18th St, Ste B, San Francisco, CA
+          94114.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
