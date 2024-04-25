a4c41faca2dd4dfae60e96f8263eb33b5f24bd67
Object-ions <moshikoatia@gmail.com>
2024-04-25 15:31:44 -0700

    Successfully fetch a collection from the DB

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,7 +1,55 @@
 import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../Firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
 
 export const Collection = () => {
+  const [collectionProducts, setCollectionProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const params = useParams();
+
+  useEffect(() => {
+    const fetchCollection = async () => {
+      try {
+        const collectionRef = collection(db, 'products');
+
+        const q = query(
+          collectionRef,
+          where('collection', '==', params.collectionName),
+          limit(10)
+        );
+
+        const querySnap = await getDocs(q);
+
+        const products = [];
+
+        querySnap.forEach((doc) => {
+          console.log(doc.data());
+          products.push({ id: doc.id, data: doc.data() });
+        });
+
+        setCollectionProducts(products);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        toast.error('Could not fetch the collection');
+        setLoading(false);
+      }
+    };
+
+    fetchCollection();
+  }, [params.collectionName]);
 
   return (
     <div>
