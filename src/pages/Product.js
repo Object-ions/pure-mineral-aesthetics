07f8ba766c788df286a59d6390c5ce720d38eb26
Object-ions <moshikoatia@gmail.com>
2024-04-26 14:32:59 -0700

    Successfully fetch and display 'Product' data from DB

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,6 +4,7 @@ import { getDoc, doc } from 'firebase/firestore';
 import { db } from '../Firebase.config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
+import { toast } from 'react-toastify';
 
 const Product = () => {
   const [product, setProduct] = useState(null);
@@ -20,7 +21,6 @@ const Product = () => {
 
       if (docSnap.exists()) {
         setProduct(docSnap.data());
-        console.log(docSnap.data());
         setLoading(false);
       }
     };
@@ -28,7 +28,62 @@ const Product = () => {
     fetchProduct();
   }, [navigate, params.productId]);
 
-  return <div>Product</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  const {
+    collectionName,
+    imgUrls,
+    name,
+    description,
+    offer,
+    discountedPrice,
+    regularPrice,
+    benefits,
+  } = product || {};
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setSharedLinkCopied(true);
+    toast.success('Link copied');
+  };
+
+  return (
+    <div className="product">
+      <div className="product-container">
+        <div className="share-icon-div">
+          <img src={shareIcon} alt="Share Icon" onClick={copyLink} />
+          <p>Share</p>
+        </div>
+        <div className="product-image">
+          <div className="image-wrapper">
+            <img src={imgUrls[0]} alt={name} />
+          </div>
+        </div>
+        <div
+          className="tags"
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          <p>Anti-Wrinkles</p>
+          <p>Firming</p>
+          <p>Lifting</p>
+        </div>
+        <p>{name}</p>
+        <p>{description}</p>
+        {offer && <p className="offer">Special Offer!</p>}
+        {offer ? <p>$ {discountedPrice}</p> : <p>$ {regularPrice}</p>}
+        <div className="benefits">
+          <ul>
+            {benefits.map((benefit, index) => (
+              <li key={index}>{benefit}</li>
+            ))}
+          </ul>
+        </div>
+        <Link onClick={() => navigate(-1)}>Go Back</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
