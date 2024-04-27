4c192a549ddf8c0ac54797929d942ec78a5ecd66
Object-ions <moshikoatia@gmail.com>
2024-04-26 23:27:31 -0700

    Update scripts to upload to Firestore from JSON file. Install dependency. successfully upload to FB

--- src/scripts/uploadData.js
+++ src/scripts/uploadData.js
@@ -24,7 +24,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Read the JSON file - ensure you have the correct path to your JSON file
-const jsonFilePath = '../src/data/treatmentsData.json'; // Update the path as necessary
+const jsonFilePath = './src/data/product_scraper.json';
 const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
 
 // Helper function to upload data to Firestore
