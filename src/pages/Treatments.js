9f8b17c9fbdb6fbdda1310ee0ec764bdf5c9a7b1
Object-ions <moshikoatia@gmail.com>
2024-04-28 10:59:44 -0700

    Update Treatments and their data, update content structure and update syntax and presentation

--- src/pages/Treatments.js
+++ src/pages/Treatments.js
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom';
 import treatmentsData from '../data/treatmentsData.json';
+import bgGrey from '../assets/website-images/bg-images/bg-grey.jpg';
 
 const Treatments = () => {
   return (
     <div className="treatments">
       <h2>Our Treatments</h2>
       <div className="treatments-container">
-        {treatmentsData.map((treatment) => (
-          <div key={treatment.id} className="treatments-card">
-            <h3>{treatment.name}</h3>
-            <p>{treatment.description}</p>
-            <p className="treatment-price">${treatment.price}</p>
-            <Link to="/" className="btn">
-              Book This Treatment
-            </Link>
+        {treatmentsData.map(({ name, id, description, price }) => (
+          <div
+            key={id}
+            className="treatments-card"
+            style={{ background: `url(${bgGrey})` }}
+          >
+            <div className="treatment-content">
+              <h3>{name}</h3>
+              <p>{description}</p>
+              <p>${price}</p>
+
+              <Link to="/" className="btn">
+                Book This Treatment
+              </Link>
+            </div>
           </div>
         ))}
       </div>
