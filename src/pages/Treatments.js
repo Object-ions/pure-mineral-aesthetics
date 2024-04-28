da7ce6e56ab0feb827fd2607c9ed9cc1a5c2efd1
Object-ions <moshikoatia@gmail.com>
2024-04-28 12:25:08 -0700

    Add assets and update presentation for Treatments and Collections

--- src/pages/Treatments.js
+++ src/pages/Treatments.js
@@ -2,6 +2,24 @@ import { Link } from 'react-router-dom';
 import treatmentsData from '../data/treatmentsData.json';
 import bgGrey from '../assets/website-images/bg-images/bg-grey.jpg';
 
+import img1 from '../assets/website-images/treatments/1.png';
+import img2 from '../assets/website-images/treatments/2.png';
+import img3 from '../assets/website-images/treatments/3.png';
+import img4 from '../assets/website-images/treatments/4.png';
+import img5 from '../assets/website-images/treatments/5.png';
+import img6 from '../assets/website-images/treatments/6.png';
+import img7 from '../assets/website-images/treatments/7.png';
+
+const images = {
+  1: img1,
+  2: img2,
+  3: img3,
+  4: img4,
+  5: img5,
+  6: img6,
+  7: img7,
+};
+
 const Treatments = () => {
   return (
     <div className="treatments">
@@ -13,6 +31,10 @@ const Treatments = () => {
             className="treatments-card"
             style={{ background: `url(${bgGrey})` }}
           >
+            <div
+              className="treatment-image"
+              style={{ background: `url(${images[id]})` }}
+            ></div>
             <div className="treatment-content">
               <h3>{name}</h3>
               <p>{description}</p>
